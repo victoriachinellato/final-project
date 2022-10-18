ba17a73d4469a4b5d9fd2902879fe9b7def337b1
Victoria Chinellato <victoriach@live.com.ar>
2022-10-18 12:54:20 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,13 @@
 
 </template>
 <script setup>
+import {createClient} from '@supabase/supabase-js'
 import NavBar from '../src/components/NavBar.vue'
 console.log(import.meta.env.BASE_URL)
+
+// esto nos crea un objeto que nos permtite interactuar con supabase:
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+console.log(supabase)
 </script>
 <style scoped>
 </style>
